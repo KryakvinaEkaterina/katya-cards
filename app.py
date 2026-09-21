72d7962ccba19fdb27b55a7f771fbca5a52b3b37
Ekaterina_Kryakvina <kryakvina.yekaterina@list.ru>
2026-09-21 20:52:19 +0300

    app.py

--- app.py
+++ app.py
@@ -375,4 +375,7 @@ def export():
                      mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+   import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
